ebd1f5a67ac1fc48bb6a860d61d0e3aebc39604a
Justin Hanlin <justintosh@justins-mbp-2.lan>
2020-08-29 16:49:23 +0800

    Added Author class + .gitignore

--- article.py
+++ article.py
@@ -1,5 +1,149 @@
-from .db_class import DBClass, SettingHandler, SettingType
 import re
+
+
+class Author:
+
+    def __init__(self):
+        self._aid = ''
+        self._sequence = ''
+        self._first_name = ''
+        self._middle_name = ''
+        self._last_name = ''
+        self._email = ''
+        self._affiliation = ''
+        self._primary_contact = False
+
+    #########
+    @property
+    def aid(self):
+        return self._aid
+
+    @aid.setter
+    def aid(self, value):
+        self._aid = value
+
+    @aid.deleter
+    def aid(self):
+        del self._aid
+
+    #########
+    @property
+    def sequence(self):
+        return self._sequence
+
+    @sequence.setter
+    def sequence(self, value):
+        self._sequence = value
+
+    @sequence.deleter
+    def sequence(self):
+        del self._sequence
+
+    #########
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, fn):
+        self._first_name = fn
+
+    @first_name.deleter
+    def first_name(self):
+        del self._first_name
+
+    #########
+    @property
+    def middle_name(self):
+        return self._middle_name
+
+    @middle_name.setter
+    def middle_name(self, mn):
+        self._middle_name = mn
+
+    @middle_name.deleter
+    def middle_name(self):
+        del self._middle_name
+
+    #########
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, ln):
+        self._last_name = ln
+
+    @last_name.deleter
+    def last_name(self):
+        del self._last_name
+
+    #########
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        self._email = value
+
+    @email.deleter
+    def email(self):
+        del self._email
+
+    #########
+    @property
+    def affiliation(self):
+        return self._affiliation
+
+    @affiliation.setter
+    def affiliation(self, aff):
+        self._affiliation = aff
+
+    @affiliation.deleter
+    def affiliation(self):
+        del self._affiliation
+
+    #########
+    @property
+    def primary_contact(self):
+        return self._primary_contact
+
+    @primary_contact.setter
+    def primary_contact(self, value):
+        if value == '1':
+            value = True
+        elif value == '0':
+            value = False
+
+        self._primary_contact = value
+
+    @primary_contact.deleter
+    def primary_contact(self):
+        del self._primary_contact
+
+    def get_full_name(self):
+
+        is_chinese = re.compile(r'[\u4e00-\u9fff]+')
+
+        if is_chinese.findall(self.first_name) \
+                or is_chinese.findall(self.last_name):
+            if self.middle_name:
+                full_name = '{}•{}'.format(self.first_name,
+                                           self.last_name)
+            else:
+                full_name = self.last_name + self.first_name
+        else:
+            if self.middle_name:
+                full_name = '{} {} {}'.format(self.first_name,
+                                              self.middle_name,
+                                              self.last_name)
+            else:
+                full_name = '{} {}'.format(self.first_name,
+                                           self.last_name)
+
+        return full_name
+
 
 class Article:
 
@@ -147,4 +291,3 @@ class Article:
     @copyright_holder.deleter
     def copyright_holder(self):
         del self._copyright_holder
-        
